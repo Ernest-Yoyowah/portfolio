db468866776ea60cf1f5ddd0ac7c32bda88e4ed2
ernkeyz <exconzy26@gmail.com>
2023-09-02 14:43:56 +0000

    portfolio update

--- src/container/Footer/Footer.jsx
+++ src/container/Footer/Footer.jsx
@@ -55,7 +55,7 @@ const Footer = () => {
 
   return (
     <>
-      <h2 className="head-text">Take a coffee & chat with me</h2>
+      <h2 className="head-text">Contact Me</h2>
 
       <div className="app__footer-cards">
         <div className="app__footer-card ">
@@ -85,14 +85,14 @@ const Footer = () => {
           </div>
         </div>
 
-      
+
       {!isFormSubmitted ? (
         <div className="app__footer-form app__flex">
           <div className="app__flex">
-            <input className="p-text" type="text" placeholder="Your Name" name="username" value={username} onChange={handleChangeInput} />
+            <input className="p-text" type="text" placeholder="Your Name" name="username" value={username} onChange={handleChangeInput} required />
           </div>
           <div className="app__flex">
-            <input className="p-text" type="email" placeholder="Your Email" name="email" value={email} onChange={handleChangeInput} />
+            <input className="p-text" type="email" placeholder="Your Email" name="email" value={email} onChange={handleChangeInput} required/>
           </div>
           <div>
             <textarea
@@ -101,6 +101,7 @@ const Footer = () => {
               value={message}
               name="message"
               onChange={handleChangeInput}
+              required
             />
           </div>
           <button type="button" className="p-text" onClick={handleSubmit}>{!loading ? 'Send Message' : 'Sending...'}</button>
