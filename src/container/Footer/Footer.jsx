52e5fc6fb7c2f393d2d6eaf76a100031e7cff24f
ernkeyz <exconzy26@gmail.com>
2023-10-06 11:35:17 +0000

    updated-resume

--- src/container/Footer/Footer.jsx
+++ src/container/Footer/Footer.jsx
@@ -36,7 +36,6 @@ const Footer = () => {
       },
       'Vd0tZePXYFCoCiL13'
     );
-    
 
     const contact = {
       _type: 'contact',
@@ -55,7 +54,7 @@ const Footer = () => {
 
   return (
     <>
-      <h2 className="head-text">Contact Me</h2>
+      <h2 className="head-text">Contact</h2>
 
       <div className="app__footer-cards">
         <div className="app__footer-card ">
--- src/container/Header/Header.jsx
+++ src/container/Header/Header.jsx
@@ -33,8 +33,8 @@ const Header = () => (
         </div>
 
         <div className="tag-cmp app__flex">
+          <p className="p-text">Software Engineering Major</p>
           <p className="p-text">Front-end Developer</p>
-          <p className="p-text">UX/UI Enthusiast</p>
         </div>
       </div>
     </motion.div>
