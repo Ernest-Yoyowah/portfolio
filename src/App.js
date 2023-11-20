75da424e78569dc86e63ef08ab19dd84e7742beb
ernkeyz <exconzy26@gmail.com>
2023-11-20 13:48:39 +0000

    Educational Section-Front-End

--- src/App.js
+++ src/App.js
@@ -13,7 +13,7 @@ const App = () => {
       <About />
       <Work />
       <Skills />
-      {/* <Education /> */}
+      <Education />
       <Testimonial />
       <Footer />
     </div>
---
+++ src/container/Education/Education.jsx
@@ -0,0 +1,54 @@
+// Education.jsx
+import React, { useState, useEffect } from "react";
+import { motion } from "framer-motion";
+import { Tooltip } from "react-tooltip";
+
+import { AppWrap, MotionWrap } from "../../wrapper";
+import { urlFor, client } from "../../client";
+import "./Education.scss";
+
+const Education = () => {
+  const [educations, setEducations] = useState([]);
+
+  useEffect(() => {
+    const query = '*[_type == "educations"]';
+
+    client.fetch(query).then((data) => {
+      setEducations(data);
+    });
+  }, []);
+
+  return (
+    <>
+      <h2 className="head-text">Education</h2>
+
+      <div className="app__education-container">
+        <motion.div className="app__education-list">
+          {educations.map((education) => (
+            <motion.div
+              whileInView={{ opacity: [0, 1] }}
+              transition={{ duration: 0.5 }}
+              className="app__education-item app__flex"
+              key={education.institution}
+            >
+              <div className="app__flex">
+                <img src={urlFor(education.icon)} alt={education.institution} />
+              </div>
+              <div className="app__education-details">
+                <h3>{education.institution}</h3>
+                <p>{`Degree: ${education.degree}`}</p>
+                <p>{`Year: ${education.year}`}</p>
+              </div>
+            </motion.div>
+          ))}
+        </motion.div>
+      </div>
+    </>
+  );
+};
+
+export default AppWrap(
+  MotionWrap(Education, "app__education"),
+  "education",
+  "app__whitebg"
+);
