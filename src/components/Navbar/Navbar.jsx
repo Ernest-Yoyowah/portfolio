a46a0f9a9e656c7f715c67f5c6cbba71cf8f9bf9
ernkeyz <exconzy26@gmail.com>
2024-03-18 19:34:53 +0000

    upadting navbar

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -11,7 +11,8 @@ const Navbar = () => {
   return (
     <nav className="app__navbar">
       <a className="app__navbar-logo" href="/">
-        <img src={images.logo} alt="logo" />
+        {/* <img src={images.logo} alt="logo" /> */}
+        ERNEST
       </a>
       <ul className="app__navbar-links">
         {["home", "about", "work", "skills", "contact"].map((item) => (
