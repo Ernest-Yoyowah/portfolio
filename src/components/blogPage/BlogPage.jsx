ebc3342b495fc97a6006f9276d4f211d22085196
ernkeyz <exconzy26@gmail.com>
2024-03-19 13:35:42 +0000

    updating blog page

--- src/components/blogPage/BlogPage.jsx
+++ src/components/blogPage/BlogPage.jsx
@@ -2,7 +2,6 @@ import React, { useState, useEffect } from "react";
 import { motion } from "framer-motion";
 import { Link } from "react-router-dom";
 import { client } from "../../client";
-import { images } from "../../constants";
 import "./BlogPage.scss";
 
 const BlogPage = () => {
