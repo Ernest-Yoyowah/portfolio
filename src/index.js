082fdc4f8ae798edf6026b6c7f4e799ace141b86
ernkeyz <exconzy26@gmail.com>
2024-03-19 09:48:06 +0000

    Replaced ReactDOM.render with createRoot to comply with React 18's deprecation of ReactDOM.render. Now the application utilizes the recommended rendering method.

--- src/index.js
+++ src/index.js
@@ -1,7 +1,10 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
+import React from "react";
+import { createRoot } from "react-dom/client";
 
-import App from './App';
-import './index.css';
+import App from "./App";
+import "./index.css";
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const root = document.getElementById("root");
+const rootElement = createRoot(root);
+
+rootElement.render(<App />);
