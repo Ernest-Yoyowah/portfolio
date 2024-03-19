5b11ae394cd93a9094d4c41ea2c2a468de3f3026
ernkeyz <exconzy26@gmail.com>
2024-03-19 08:51:38 +0000

    adding react router

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React from "react";
+import { BrowserRouter } from "react-dom";
 
 import { About, Footer, Header, Skills, Testimonial, Work } from "./container";
 import { Navbar } from "./components";
